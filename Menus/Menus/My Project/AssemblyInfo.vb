79f4583273f2af99bf477c49c6b69f39e26edafe
KYLIE CHAPMAN <Kacha964@learner.dvusd.org>
2024-01-08 08:24:50 -0700

    Textbox- MultiLine

---
+++ Menus/Menus/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Menus")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Deer Valley USD")>
+<Assembly: AssemblyProduct("Menus")>
+<Assembly: AssemblyCopyright("Copyright © Deer Valley USD 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9bff09a0-9905-4c62-81ae-3988c99dc9c5")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
