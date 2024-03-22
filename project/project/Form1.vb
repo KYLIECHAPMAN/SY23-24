ce531f03a736d70ce4e933dcbfdec053fb38c2a6
KYLIE CHAPMAN <Kacha964@learner.dvusd.org>
2024-03-22 07:39:09 -0700

    Changes

---
+++ project/project/Form1.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Reflection.Emit
+Imports System.Windows
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Dim f2 As New Form2
+        f2.ShowDialog()
+        Label1.Text = f2.ClothingNumericUpDown1.Value
+        Me.Show()
+    End Sub
+End Class
